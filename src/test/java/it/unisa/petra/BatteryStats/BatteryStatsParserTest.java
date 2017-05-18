d3e73baba719a0481a3195bcfb3afd477b706153
dardin88 <linkin88mail@gmail.com>
2017-05-18 19:39:06 +0200

    Add BatteryStatsParser test.

---
+++ src/test/java/it/unisa/petra/BatteryStats/BatteryStatsParserTest.java
@@ -0,0 +1,30 @@
+package it.unisa.petra.BatteryStats;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author dardin88
+ */
+public class BatteryStatsParserTest {
+
+    /**
+     * Test of parseFile method of class BatteryStatsParser.
+     *
+     */
+    @Test
+    public void testNumberOfEnergyInfo() throws Exception {
+        String fileName = "src/test/resources/batterystats_example";
+        int traceviewStart = 0;
+        int traceviewLength = 10000;
+        ArrayList<EnergyInfo> result = BatteryStatsParser.parseFile(fileName, traceviewStart, traceviewLength);
+        assertEquals(3, result.size());
+        assertEquals(5, result.get(0).getDevices().size());
+        assertEquals(6, result.get(1).getDevices().size());
+        assertEquals(6, result.get(2).getDevices().size());
+    }
+
+}
