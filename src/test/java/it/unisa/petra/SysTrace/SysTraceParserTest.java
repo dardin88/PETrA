b699b8a557e01165ea2bd1de6eeb235d215ad4de
dardin88 <linkin88mail@gmail.com>
2017-05-18 20:43:08 +0200

    Add SysTrace test.

---
+++ src/test/java/it/unisa/petra/SysTrace/SysTraceParserTest.java
@@ -0,0 +1,23 @@
+package it.unisa.petra.SysTrace;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author dardin88
+ */
+public class SysTraceParserTest {
+
+    @Test
+    public void testNumberOfEnergyInfo() throws Exception {
+        String fileName = "src/test/resources/systrace";
+        int traceviewStart = 0;
+        int traceviewLength = 10000;
+        SysTrace result = SysTraceParser.parseFile(fileName, traceviewStart, traceviewLength);
+        assertEquals(25123208, result.getSystraceStartTime());
+        assertEquals(25123208, result.getFrequency().get(0).getTime());
+        assertEquals(486000, result.getFrequency().get(0).getValue());
+    }
+
+}
