29d76c4057ec7d9baa5fee8f5e1d5c56706d408a
dardin88 <linkin88mail@gmail.com>
2017-05-21 12:08:16 +0200

    Add a test to Process class.

---
+++ src/test/java/it/unisa/petra/core/ProcessTest.java
@@ -0,0 +1,32 @@
+package it.unisa.petra.core;
+
+import it.unisa.petra.core.traceview.TraceLine;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author dardin88
+ */
+public class ProcessTest {
+
+    @Test
+    public void testParser() throws Exception {
+        String traceviewFilename = "src/test/resources/tracedump_example";
+        String batteryStatsFilename = "src/test/resources/batterystats_example";
+        String systrace_example = "src/test/resources/systrace_example";
+        String powerProfileFile = "src/test/resources/power_profile_example.xml";
+
+        String appName = "ch.smalltech.battery";
+
+        Process process = new Process();
+
+        List<TraceLine> traceLines = process.parseAndAggregateResults(traceviewFilename, batteryStatsFilename, systrace_example, powerProfileFile, appName, 1);
+
+        assertEquals(traceLines.get(0).getTimeLength(), 0.000011101, 0.000000001);
+        assertEquals(traceLines.get(0).getConsumption(), 0.000004905, 0.000000001);
+    }
+
+}
